9873f00bf86865228c22a085e93aedc561a197ff
Caio Costa <caiocvtc0@gmail.com>
2020-11-12 23:21:29 -0300

    Add some more behaviour to Chips

--- app/src/main/java/br/com/caiodev/newmaterialdesigntest/MainActivity.kt
+++ app/src/main/java/br/com/caiodev/newmaterialdesigntest/MainActivity.kt
@@ -1,5 +1,28 @@
 package br.com.caiodev.newmaterialdesigntest
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.chip.Chip
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setChipListener(firstChip)
+        setChipListener(chip)
+    }
+
+    private fun setChipListener(chip: Chip) {
+        chip.apply {
+            setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    setTextColor(ContextCompat.getColor(applicationContext, android.R.color.white))
+                } else {
+                    setTextColor(ContextCompat.getColor(applicationContext, android.R.color.black))
+                }
+            }
+        }
+    }
+}
